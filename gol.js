8c15e3e38375bbde50b34fbe7c2d7749e7f4a4bd
Kevin Rutherford <kevin@rutherford-software.com>
2013-11-24 21:51:30 +0000

    Working version!

--- gol.js
+++ gol.js
@@ -1,21 +1,33 @@
 var dim = 5;
+var cols = 100;
 
 var cellData = [];
-for (var i = 0; i < 100; i++) {
-  for (var j = 0; j < 100; j++) {
-    cellData.push({x: i, y: j, state: 'dead'});
+var state = 'dead';
+for (var i = 0; i < cols; i++) {
+  //state = (state == 'alive') ? 'dead' : 'alive';
+  for (var j = 0; j < cols; j++) {
+    cellData.push({x: i, y: j, state: state});
   }
 }
 
 function gen(cells, i) {
+  var neighbours = [
+    cells[i-cols-1], cells[i-cols], cells[i-cols+1],
+    cells[i-1], cells[i+1],
+    cells[i+cols-1], cells[i+cols], cells[i+cols+1]
+  ].filter(function(cell) { return (typeof cell != 'undefined'); });
+  var numal = neighbours.filter(function(cell) { return (cell.state == 'alive'); }).length;
   var cell = cells[i];
-  var ns = (cell.state == 'dead') ? 'alive' : 'dead';
-  return ns;
+  if (numal == 3)
+    return 'alive';
+  if (numal == 2 && cell.state == 'alive')
+    return 'alive';
+  return 'dead';
 }
 
 function generation(cells) {
   var nextgen = [];
-  for (var i = 0; i < 100*100; i++) {
+  for (var i = 0; i < cols*cols; i++) {
     var cell = cells[i];
     var ns = gen(cells, i);
     nextgen.push({x: cell.x, y: cell.y, state: ns});
@@ -24,7 +36,7 @@ function generation(cells) {
 }
 
 function mouseout(d) {
-  d.state = 'live';
+  d.state = 'alive';
 }
 
 function golUpdate(cells) {
@@ -46,11 +58,11 @@ function golUpdate(cells) {
 
 function gol_start() {
   var svg = d3.select('#gol').append('svg:svg')
-    .attr('width', 100 * dim)
-    .attr('height', 100 * dim);
+    .attr('width', cols * dim)
+    .attr('height', cols * dim);
 
   setInterval(function() {
     golUpdate(cellData);
-  }, 1000);
+  }, 500);
 };
 
