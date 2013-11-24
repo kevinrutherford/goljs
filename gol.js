9bbd612204891df25809b1749cd2f739cda3121a
Kevin Rutherford <kevin@rutherford-software.com>
2013-11-24 20:23:47 +0000

    Mouse movement initializes some live cells

--- gol.js
+++ gol.js
@@ -1,24 +1,37 @@
-function gol_start(div) {
-  var svg = d3.select(div).append('svg:svg')
-    .attr('width', 500)
-    .attr('height', 500);
-
-  var cellData = [];
-  for (var i = 0; i < 100; i++) {
-    for (var j = 0; j < 100; j++) {
-      cellData.push({x: i, y: j, state: 'dead'});
-    }
+var cellData = [];
+for (var i = 0; i < 100; i++) {
+  for (var j = 0; j < 100; j++) {
+    cellData.push({x: i, y: j, state: 'dead'});
   }
+}
 
+function mouseout(d) {
+  d.state = 'live';
+}
+
+function golUpdate(cellData) {
+  var svg = d3.select('svg');
   var cells = svg.selectAll("rect")
-    .data(cellData)
-    .enter()
+    .data(cellData);
+
+  cells.enter()
     .append("rect");
 
   cells.attr("x", function(d) { return d.x * 3; })
     .attr("y", function(d) { return d.y * 3; })
     .attr("width", 3)
     .attr("height", 3)
-    .attr("class", 'cell');
+    .attr("class", function(d) { return d.state; })
+    .on('mouseout', mouseout);
+}
+
+function gol_start() {
+  var svg = d3.select('#gol').append('svg:svg')
+    .attr('width', 500)
+    .attr('height', 500);
+
+  setInterval(function() {
+    golUpdate(cellData);
+  }, 1000);
 };
 
