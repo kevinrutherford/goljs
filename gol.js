32bfaa8abd26688656085555fbd4eddf8cf4e387
Kevin Rutherford <kevin@rutherford-software.com>
2013-11-24 20:53:44 +0000

    The cells alternate between alive and dead

--- gol.js
+++ gol.js
@@ -1,3 +1,5 @@
+var dim = 5;
+
 var cellData = [];
 for (var i = 0; i < 100; i++) {
   for (var j = 0; j < 100; j++) {
@@ -5,11 +7,28 @@ for (var i = 0; i < 100; i++) {
   }
 }
 
+function gen(cells, i) {
+  var cell = cells[i];
+  var ns = (cell.state == 'dead') ? 'alive' : 'dead';
+  return ns;
+}
+
+function generation(cells) {
+  var nextgen = [];
+  for (var i = 0; i < 100*100; i++) {
+    var cell = cells[i];
+    var ns = gen(cells, i);
+    nextgen.push({x: cell.x, y: cell.y, state: ns});
+  }
+  return nextgen;
+}
+
 function mouseout(d) {
   d.state = 'live';
 }
 
-function golUpdate(cellData) {
+function golUpdate(cells) {
+  cellData = generation(cells);
   var svg = d3.select('svg');
   var cells = svg.selectAll("rect")
     .data(cellData);
@@ -17,18 +36,18 @@ function golUpdate(cellData) {
   cells.enter()
     .append("rect");
 
-  cells.attr("x", function(d) { return d.x * 3; })
-    .attr("y", function(d) { return d.y * 3; })
-    .attr("width", 3)
-    .attr("height", 3)
+  cells.attr("x", function(d) { return d.x * dim; })
+    .attr("y", function(d) { return d.y * dim; })
+    .attr("width", dim)
+    .attr("height", dim)
     .attr("class", function(d) { return d.state; })
     .on('mouseout', mouseout);
 }
 
 function gol_start() {
   var svg = d3.select('#gol').append('svg:svg')
-    .attr('width', 500)
-    .attr('height', 500);
+    .attr('width', 100 * dim)
+    .attr('height', 100 * dim);
 
   setInterval(function() {
     golUpdate(cellData);
